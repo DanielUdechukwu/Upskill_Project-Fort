7d70b1d929861ce03af2ff669ee3956de9069963
Daniel Udechukwu <danieludechukwu117@gmail.com>
2023-07-14 12:05:50 +0100

    :construction: Properties page: major work done

--- js/script.js
+++ js/script.js
@@ -5,3 +5,6 @@ let currentDate = new Date();
 let year = currentDate.getFullYear();
 console.log(year);
 date.innerHTML = year;
+
+const myP = document.getElementById('myP');
+myP.innerHTML = 'MICHAELss'
