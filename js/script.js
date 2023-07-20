d4c37f5e38f95a00a20ad4a65f3432fbc315a2b6
Daniel Udechukwu <danieludechukwu117@gmail.com>
2023-07-20 14:47:48 +0100

    :construction: Started Responsive Layout

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 
-document.getElementById("date")
+  // document.getElementById("date")
 
-let currentDate = new Date();
-let year = currentDate.getFullYear();
-console.log(year);
-date.innerHTML = year;
+  // let currentDate = new Date();
+  // let year = currentDate.getFullYear();
+  // console.log(year);
+  // date.innerHTML = year;
 
-const myP = document.getElementById('myP');
-myP.innerHTML = 'MICHAELss'
+  // const myP = document.getElementById('myP');
+  // myP.innerHTML = 'MICHAELss'
